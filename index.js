9db4f2a63c34f0c20156cfc7ffe13d88639c4587
Chitty Liu <d2921118@gmail.com>
2018-12-09 17:46:33 +0000

    caculating the differenceof mean and amount spend by each person

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-[
+splitMoney = input => {
+  let payers = [];
+  let spenders = [];
+  input.forEach(e => payers.push(e.Title, e.Payer));
+  input.forEach(e => console.log(e["Title"], e["Payer"]));
+  // [ 'Lunch', 'Tommen', 'Movie', 'Kelly' ]
+  input.map(x => Object.keys(x.Spend));
+  // [ [ 'Kelly', 'Sam', 'Ola', 'Tommen', 'Sandy' ],
+  //   [ 'Kelly', 'Sam', 'Ola', 'Tommen', 'Sandy' ] ]
+  input.map(x => Object.values(x.Spend));
+  //   [ [ 5.5, 10, 15, 10, 5 ], [ 10, 10, 10, 10, 10 ] ]
+  input.map(x => Object.keys(x.Spend).map(y => spenders.push(y)));
+  //   [
+  //     "Kelly",
+  //     "Sam",
+  //     "Ola",
+  //     "Tommen",
+  //     "Sandy",
+  //     "Kelly",
+  //     "Sam",
+  //     "Ola",
+  //     "Tommen",
+  //     "Sandy"
+  //   ];
+  spenders.filter((v, i) => spenders.indexOf(v) === i);
+  // [ 'Kelly', 'Sam', 'Ola', 'Tommen', 'Sandy' ]
+  let headcounts = spenders.filter((v, i) => spenders.indexOf(v) === i).length;
+};
+const input = [
   {
-    Title: Lunch,
+    Title: "Lunch",
     Spend: { Kelly: 5.5, Sam: 10, Ola: 15, Tommen: 10, Sandy: 5 },
     Total: 45.5,
-    Payer: Tommen
+    Payer: "Tommen"
   },
   {
-    Title: Movie,
+    Title: "Movie",
     Spend: { Kelly: 10, Sam: 10, Ola: 10, Tommen: 10, Sandy: 10 },
     Total: 50,
-    Payer: Kelly
+    Payer: "Kelly"
   }
 ];
